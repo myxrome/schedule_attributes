8911c420f78a3462760af4cbf465cae18741174f
Andrew Vit <andrew@avit.ca>
2013-01-07 00:11:50 -0800

    Evaluate default_schedule when called
    Fixes scenarios when the model is defined under a different Time.zone
    e.g. across different test examples.

--- lib/schedule_attributes/active_record.rb
+++ lib/schedule_attributes/active_record.rb
@@ -8,13 +8,17 @@ module ScheduleAttributes::ActiveRecord
   module ClassMethods
     attr_accessor :schedule_field
     attr_accessor :default_schedule
+
+    def default_schedule
+      @default_schedule || ScheduleAttributes.default_schedule
+    end
   end
 
   module Sugar
     def has_schedule_attributes(options={:column_name => :schedule})
       options[:column_name] ||= ScheduleAttributes::DEFAULT_ATTRIBUTE_KEY
       @schedule_field = options[:column_name]
-      @default_schedule = options[:default_schedule] || ScheduleAttributes.default_schedule
+      @default_schedule = options[:default_schedule] if options.has_key?(:default_schedule)
       serialize @schedule_field, ScheduleAttributes::Serializer
       include ScheduleAttributes::ActiveRecord
     end
