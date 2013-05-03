f813a3c8863825ea080248ab405ffa61b6cb93e9
Andrew Vit <andrew@avit.ca>
2013-05-02 18:03:32 -0700

    More defensive serialization to catch a strange bug
    ActiveRecord would sometimes re-serialize the already-dumped YAML
    string back to the database, which would then re-re-serialize on the
    next save, and so on creating a chain of "--- \\\\n\"--- \\n ...".
    Best guess is that somehow querying a bad value just passes the string
    through to the model instead of a schedule, which then gets passed
    back to dump when saving. Not sure if this addresses the root of the
    problem since it was happening to records with previously known good
    schedule values.

--- lib/schedule_attributes/serializer.rb
+++ lib/schedule_attributes/serializer.rb
@@ -1,11 +1,30 @@
+require 'ice_cube'
+
 module ScheduleAttributes
   class Serializer
+
+    # Only load YAML serializations that are present, not empty strings
+    # An invalid value can raise an error that gets caught by ActiveRecord
+    # and results in the original string getting passed through. This allows
+    # it to be saved back unchanged if necessary.
+    #
     def self.load(yaml)
-      IceCube::Schedule.from_yaml(yaml) if yaml
+      IceCube::Schedule.from_yaml(yaml) if yaml.present?
     end
 
+    # This should normally receive a Schedule object.
+    # In some unknown circumstance that I can't reproduce, it would save an
+    # already-dumped serialized YAML Schedule string into a YAML string
+    # wrapper, effectively double-bagging the YAML. I only assume it receives a
+    # String when loading a bad YAML value from the database, which then
+    # propagates and re-re-serializes itself into YAML on every save.
+    #
     def self.dump(schedule)
-      schedule.to_yaml if schedule
+      case schedule
+      when IceCube::Schedule then schedule.to_yaml
+      when String            then schedule
+      end
     end
+
   end
 end
