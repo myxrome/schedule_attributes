94dfadd12cf027c38c031afb22dec41e73c7e86d
Andrew Vit <andrew@avit.ca>
2013-05-07 15:02:07 -0700

    Update dependency to IceCube 0.10

--- lib/schedule_attributes.rb
+++ lib/schedule_attributes.rb
@@ -1,3 +1,18 @@
 require 'schedule_attributes/model'
 require 'schedule_attributes/railtie' if defined? Rails
 require 'schedule_attributes/form_builder' if defined? Formtastic
+
+if defined? Arel
+  module Arel::Visitors
+    class ToSql
+
+      # Allow schedule occurrences to be used directly as Time objects in
+      # ActiveRecord queries
+      #
+      def visit_IceCube_Occurrence(value)
+        quoted(value.to_time)
+      end
+
+    end
+  end
+end
