f376abd39cfb9d88a2e03884925d2b3f42e46b44
Aleksey Zolotov <myxrome@gmail.com>
2020-09-25 14:08:13 +0400

    Update ice_cube.rb
    * fixed nil value comparison.

--- lib/schedule_attributes/extensions/ice_cube.rb
+++ lib/schedule_attributes/extensions/ice_cube.rb
@@ -1,11 +1,13 @@
 class IceCube::Rule
   def ==(other)
+    return false unless other.present?
     to_hash == other.to_hash
   end
 end
 
 class IceCube::Schedule
   def ==(other)
+    return false unless other.present?
     to_hash == other.to_hash
   end
 end
